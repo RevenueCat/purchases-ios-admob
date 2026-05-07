6777e42fca135ab556058b3b53f491ad888c6323
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-05-07 19:30:03 +0000

    Release 5.72.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .library(name: "RevenueCatAdMob", targets: ["RevenueCatAdMob"])
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm.git", exact: "5.71.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm.git", exact: "5.72.0"),
         .package(
             url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
             "12.0.0"..<"14.0.0"
