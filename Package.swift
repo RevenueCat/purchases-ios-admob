33b8489e0a89d4f9cde484110d840896963b750a
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-06-18 05:49:08 +0000

    Release 5.79.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .library(name: "RevenueCatAdMob", targets: ["RevenueCatAdMob"])
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm.git", exact: "5.78.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm.git", exact: "5.79.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "13.0.0"),
         .package(
             url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
